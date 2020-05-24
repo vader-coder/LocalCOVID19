42809dd602c7ab8ab7e8045cfd0d7db7d5624ff4
Patrick Wheeler <pwheele1@swarthmore.edu>
2020-05-24 16:49:21 -0400

    added xlsx package

--- calculations.js
+++ calculations.js
@@ -1,21 +1,30 @@
 //this file contains a function that calculates
 //the number of days the database started.
-var days_since_start = function(month, date, year) {//all ints 1-12, 1-31, 2019->
+function days_since_start(month, date, year) {//all ints 1-12, 1-31, 2019->
   var days = 0;
   month--;//decrement by 1 so from 0-11 instead of 1-12
   let dPM20 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]//days per month
-  if (year == 2020) {
+  if (year > 2020) {
+    var years_btw = year - 2020;
+    days = years_btw*365;
+  }
+  if (year >= 2020) {
     for (var i = 0; i<month; i++) {//looping throgh months that aren't ours
       days+=dPM20[i];//add days in months past.
     }
     days+=date;//add date in current month, which is days into current month
   }
+  if (year <= 2019 && month != 12 && day != 31) {
+    return "Invalid"
+  }
   //if year > 2020
   return days;
 }
+module.exports.days_since_start = days_since_start;//https://www.youtube.com/watch?v=Cxo4UKpHv5s
 console.log(days_since_start(3, 13, 2020));//march 13, 2020: 73
 console.log(days_since_start(1, 1, 2020));//jan 1, 2020: 1
 console.log(days_since_start(5,13,2020));//134, as expected.
+console.log(days_since_start(6, 1, 2021));//518
 //is this a little inconsistent?
 //they say 74 days, including end date.
 //december 31 is day 0.
--- index.js
+++ index.js
@@ -4,25 +4,39 @@ var url = require('url');
 var fs = require('fs');
 var today = new Date();//https://www.w3schools.com/jsref/jsref_obj_date.asp
 var done = 0;
+var PouchDB = require('pouchdb');
+var db = new PouchDB('my_database');
+//https://pouchdb.com/download.html
+
 //last_date.json contains date that was last updated.
 //check if this is a different day from the last one.
 //readFile & writeFile are asynchronous
 
-fs.readFile('last_date.json','utf8', (err, str) => {
+/*fs.readFile('last_date.json','utf8', (err, str) => {
   if (err) {
     console.log("Error reading .json file:",err);
     return;
   }
-  try {
-    const last = JSON.parse(str);
+  //try {
+    var last = JSON.parse(str);//will contain the last date the site was updated.
     console.log("assigned JSON object to last");
-  }
+    console.log(last.month);
+    done = 1;
+  //}
   catch(err) {
     console.log("Error parsing JSON string: ", err);
   }
-});
-console.log(today.getMonth() + ' ' + today.getDate() + ' ' + today.getFullYear());//4 18 2020
-
+});*/
+var last = JSON.parse('{"day_since_start": 134,"month": 5,"date": 13,"year": 2020}');
+console.log(last.month);
+let currMonth = today.getMonth() + 1;//current month 1-12
+let currDate = today.getDate();//date 1-31
+let currYear = today.getFullYear();//year
+console.log(currMonth + ' ' + currDate + ' ' + currYear);//4 18 2020
+if (last.month != currMonth || last.date != currDate || last.year != currYear) {//if didn't update today.
+    var updates = days_since_start(currMonth, currDate, currYear) - last.days_since_start;//number of updates
+    console.log("updates: " + updates);
+}
 var reqListener = function(req, res) {
   res.writeHead(200);
   var q = url.parse(req.url, true);
